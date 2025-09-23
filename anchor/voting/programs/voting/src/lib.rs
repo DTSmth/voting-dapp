9b4769167a354da33e38ed81c5fc2fce74117329
DTSmth <ducktapedave85@gmail.com>
2025-09-23 13:53:59 -0400

    start work on voting dapp

---
+++ anchor/voting/programs/voting/src/lib.rs
@@ -0,0 +1,40 @@
+use anchor_lang::prelude::*;
+
+declare_id!("5PkWQpy2y4pBVgLQguPNakfda9dyDUDyMRBsjxvyxjWP");
+
+#[program]
+pub mod voting {
+    use super::*;
+
+    pub fn initialize_poll(ctx: Context<InitializePoll>, poll_id: u64) -> Result<()> {
+        Ok(())
+    }
+}
+
+#[derive(Accounts)]
+#[instruction(poll_id: u64)]
+pub struct InitializePoll<'info> {
+    #[account(mut)]
+    pub signer: Signer<'info>,
+    #[account(
+        init,
+        payer = signer,
+        space = 8 + Poll::INIT_SPACE,
+        seeds = [poll_id.to_le_bytes().as_ref()],
+        bump
+    )]
+    pub poll: Account<'info, Poll>,
+
+    pub system_program: Program<'info, System>,
+}
+
+#[account]
+#[derive(InitSpace)]
+pub struct Poll {
+    pub poll_id: u64,
+    #[max_len(32)]
+    pub description: String,
+    pub poll_start: u64,
+    pub poll_end: u64,
+    pub candidate_amount: u64,
+}
